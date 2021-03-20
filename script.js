d9796b4ca552cec1fd4e8bf4c568fae083932389
Oliver Temple <oliver.temple.dev@gmail.com>
2021-03-20 14:06:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ function entryScroll() {
     behavior: "smooth",
   });
 }
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-41HV3NL2Y7"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-41HV3NL2Y7');
-</script>
